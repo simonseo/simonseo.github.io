b61c8deb706268ccbef1dabeeaf7caac66af85fb
Simon Seo <simonseo.doubles@gmail.com>
2023-11-01 22:46:55 -0400

    add chruby to run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #! /bin/zsh
 
+source $(brew --prefix)/opt/chruby/share/chruby/chruby.sh
+source $(brew --prefix)/opt/chruby/share/chruby/auto.sh
 chruby 3.2.0
-bundle exec jekyll serve --host localhost --config _config_geoviz.yml --port 4000 --open-url --incremental
+bundle exec jekyll serve --host localhost --config _config_geoviz.yml --port 4000 --open-url
